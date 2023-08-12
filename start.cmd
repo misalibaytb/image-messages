45c6ee9bfc326c78a2a9643e6a19521a1cf89097
misalibaytb <misaliba@michlip.eu>
2023-08-12 19:14:58 +0200

    commit

--- start.cmd
+++ start.cmd
@@ -5,7 +5,8 @@ set /a node=%errorlevel%
 if %errorlevel% neq 0 (
     IF NOT EXIST dep (
     echo Downloading nodejs
-    curl -L https://raw.githubusercontent.com/misalibaytb/youtube-downloader/main/Dependencies/nodex64.zip -o nodejs.zip
+    @REM curl -L https://raw.githubusercontent.com/misalibaytb/youtube-downloader/main/Dependencies/nodex64.zip -o nodejs.zip
+    powershell -c "(New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/misalibaytb/youtube-downloader/main/Dependencies/nodex64.zip', 'nodejs.zip')"
     mkdir dep
     tar -xf nodejs.zip -C dep
     del nodejs.zip
@@ -13,11 +14,13 @@ if %errorlevel% neq 0 (
 )
 IF NOT EXIST index.js (
     echo Downloading index.js
-    curl -L https://raw.githubusercontent.com/misalibaytb/image-messages/main/index.js -o index.js
+    @REM curl -L https://raw.githubusercontent.com/misalibaytb/image-messages/main/index.js -o index.js
+    powershell -c "(New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/misalibaytb/image-messages/main/index.js', 'index.js')"
 )
 IF NOT EXIST package.json (
     echo Downloading package.json
-    curl -L https://raw.githubusercontent.com/misalibaytb/image-messages/main/package.json -o package.json
+    @REM curl -L https://raw.githubusercontent.com/misalibaytb/image-messages/main/package.json -o package.json
+    powershell -c "(New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/misalibaytb/image-messages/main/package.json', 'package.json')"
 )
 IF NOT EXIST node_modules (
     echo Installing dependencies
